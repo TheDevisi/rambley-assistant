5bab39dccf62fcdd8670844d0495849bb219419f
TheDevisi <syntax_samp@proton.me>
2024-06-25 15:01:01 +0300

    fixed windows install script

--- install.bat
+++ install.bat
@@ -3,12 +3,12 @@
 echo Checking if Python installed...
 python --version > nul 2>&1
 if errorlevel 1 (
-    echo Python not found. Please install Python 3.10 from official web site..
+    echo Python not found. Please install Python 3.10 from official web site.
     pause
     exit /b 1
 )
 
-echo Installing requirements from  requirements.txt...
+echo Installing requirements from requirements.txt...
 pip install -r requirements.txt
 if errorlevel 1 (
     echo Unexpected error.
@@ -16,9 +16,16 @@ if errorlevel 1 (
     exit /b 1
 )
 
-set /p API_KEY=Please, enter your API key. (see more on GitHub):
+set /p API_KEY=Please, enter your API key (see more on GitHub):
 echo Your API: %API_KEY%
 
-echo Succes.
-pause
+echo Creating or updating settings.json...
+(
+echo{
+echo    "is_first_startup": false,
+echo    "user_api_token": "%API_KEY%"
+echo}
+) > settings.json
 
+echo Success.
+pause
