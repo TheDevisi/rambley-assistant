273fb10dbe569f7d10d43b8ac8d8213b8312b112
TheDevisi <syntax_samp@proton.me>
2024-06-30 10:02:30 +0300

    now install script should work fine

--- install.bat
+++ install.bat
@@ -3,12 +3,12 @@
 echo Checking if Python installed...
 python --version > nul 2>&1
 if errorlevel 1 (
-    echo Python not found. Please install Python 3.10 from official web site..
+    echo Python not found. Please install Python 3.10 from the official website.
     pause
     exit /b 1
 )
 
-echo Installing requirements from  requirements.txt...
+echo Installing requirements from requirements.txt...
 pip install -r requirements.txt
 if errorlevel 1 (
     echo Unexpected error.
@@ -16,9 +16,25 @@ if errorlevel 1 (
     exit /b 1
 )
 
-set /p API_KEY=Please, enter your API key. (see more on GitHub):
+set /p API_KEY=Please, enter your API key (see more on GitHub):
 echo Your API: %API_KEY%
 
-echo Succes.
-pause
+echo Updating settings.json file with your API key...
 
+powershell -Command "if (Test-Path 'settings.json') { 
+    $json = Get-Content 'settings.json' | ConvertFrom-Json; 
+    $json.user_api_token = '%API_KEY%'; 
+    $json | ConvertTo-Json | Set-Content 'settings.json'; 
+} else { 
+    $json = @{ user_api_token = '%API_KEY%' }; 
+    $json | ConvertTo-Json | Set-Content 'settings.json'; 
+}"
+
+if errorlevel 1 (
+    echo Failed to update settings.json.
+    pause
+    exit /b 1
+)
+
+echo Success.
+pause
